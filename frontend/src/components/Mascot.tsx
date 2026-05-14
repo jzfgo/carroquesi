e7c9defc2d332ee34a7960ff991bd41140a284e1
Javier Zapata <javierzapata82@gmail.com>
2026-05-14 08:49:03 +0200

    feat: add mascot asset and shared Mascot component

---
+++ frontend/src/components/Mascot.tsx
@@ -0,0 +1,17 @@
+import mascotUrl from '../assets/mascot.png'
+
+interface Props {
+  size?: number
+}
+
+export function Mascot({ size = 160 }: Props) {
+  return (
+    <img
+      src={mascotUrl}
+      alt="Mascota de CarroQueSí"
+      width={size}
+      height={size}
+      style={{ objectFit: 'contain' }}
+    />
+  )
+}
